2b74dc74e707ef4e8594c036f3bc2adf04edd02b
M. Akifhan Ilgaz <akifhanilgaz@gmail.com>
2026-01-06 00:09:55 +0300

    add cors

--- services/api-gateway/cmd/main.go
+++ services/api-gateway/cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
@@ -40,6 +41,14 @@ func main() {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	})
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Requested-With",
+		AllowCredentials: true, // Cookie veya Authorization header kullanıyorsanız
+		ExposeHeaders:    "Content-Length, Content-Type",
+		MaxAge:           86400, // 24 saat
+	}))
 
 	api := app.Group("/api", logger)
 	cryptoHandler.RegisterRoutes(api)
